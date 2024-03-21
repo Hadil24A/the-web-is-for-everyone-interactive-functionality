f8a0b3bceae2bc17f6812f6f4214d9ec3d6a86e4
Hadil24A <hadil.ahmed@hva.nl>
2024-03-21 22:58:43 +0100

    new playlist dialog

--- public/main.js
+++ public/main.js
@@ -6,3 +6,21 @@ navigatie.addEventListener("click", () => {
   navigatie.classList.toggle("active");
   menu.classList.toggle("home-menu-active");
 });
+
+let openButton = document.querySelector(".makeAPlaylistBtn");
+let newPlaylist = document.querySelector(".newPlaylist");
+let submitButton = document.getElementById("submitPL");
+let playlistNameInput = document.querySelector(".newPlaylist input[type='text']");
+
+openButton.addEventListener("click", function () {
+  newPlaylist.classList.toggle("newPlaylistOpen");
+});
+
+
+playlistNameInput.addEventListener("input", () => {
+  if (playlistNameInput.value.trim() !== '') {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
