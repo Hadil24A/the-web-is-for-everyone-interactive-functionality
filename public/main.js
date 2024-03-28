bca18231a89472ce803bdda2a173d1f812833b03
Hadil24A <hadil.ahmed@hva.nl>
2024-03-29 00:59:23 +0100

    error verbeteren :

--- public/main.js
+++ public/main.js
@@ -6,3 +6,15 @@ navigatie.addEventListener("click", () => {
   navigatie.classList.toggle("active");
   menu.classList.toggle("home-menu-active");
 });
+
+let submitButton = document.getElementById("submitPL");
+let input = document.getElementById("playlistName");
+
+input =.addEventListener("keyup", (e) => {
+  let value = e.currentTarget.value;
+  submitButton.disabled = false;
+
+  if (value === "") {
+    submitButton.disabled = true;
+  }
+});
